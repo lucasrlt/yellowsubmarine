29a6be7e4a5402568263bd57261691377663ba57
GILBERT <jordan.gilbert@etu.univ-lyon1.fr>
2019-09-26 11:08:51 +0200

    remove little rect

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ if __name__ == "__main__":
 
     play = True
     while(play):
-        win.draw1()
         win.refresh()
         play = win.close()
         continue
--- src/window.py
+++ src/window.py
@@ -15,16 +15,7 @@ class Window:
     def refresh(self):
         pygame.display.flip()
         self.terrain.space.debug_draw(self.draw_options)
-    
-    def draw1(self):
-        d1 = random.randint(0, WINDOW_SIZE[0])
-        d2 = random.randint(0, WINDOW_SIZE[1])
-        r = random.randint(0,255)
-        g = random.randint(0,255)
-        b = random.randint(0,255)
 
-        pygame.draw.rect(self.screen, (r, g, b), (d1, d2, 5, 5))
-    
     def close(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
