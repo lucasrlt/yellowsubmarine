cdaf7309169c9858ba52d2e38c03fa5ffb9d39de
GILBERT <jordan.gilbert@etu.univ-lyon1.fr>
2019-09-26 09:49:42 +0200

    init window + first fonction

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from src.constants import WINDOW_SIZE
+from src.Window import *
 
 if __name__ == "__main__":
-  print(WINDOW_SIZE)
+    print(WINDOW_SIZE)
+    win = Window()
+
+    while(1):
+        win.draw1()
+        win.refresh()
+        continue
