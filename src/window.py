eabfbddec88a6041e0aa02d0086c2fd08ed1328c
PETITJEAN DAMIEN P1408987 <damien.petitjean@etu.univ-lyon1.fr>
2019-11-07 12:32:46 +0100

    Switch display to pygame instead of pymunk debug.

--- src/window.py
+++ src/window.py
@@ -1,4 +1,4 @@
-from .constants import WINDOW_SIZE
+from .constants import *
 import pygame
 import pymunk
 import pymunk.pygame_util
@@ -16,6 +16,7 @@ class Window:
         self.stats = Stats()
         self.draw_options = pymunk.pygame_util.DrawOptions(self.screen)
         pymunk.pygame_util.positive_y_is_up = False
+
 
     def draw_arrow(self, screen, colour, start, end):
         pygame.draw.line(screen,colour,start,end,4)
@@ -37,13 +38,41 @@ class Window:
             self.draw_arrow(self.screen, (255, 0, 0), (x,y),  (sub.getScreenPosition().x - sub.size, sub.getScreenPosition().y - sub.size))
             self.draw_arrow(self.screen, (255, 0, 0), (x2,y2),  (sub.getScreenPosition().x, sub.getScreenPosition().y + sub.size))
 
+    def drawLinesBoxes(self):
+        for i in range(len(self.terrain.verticesBottomList)-1):
+            pygame.draw.line(self.screen,(255,0,0),self.terrain.verticesBottomList[i],self.terrain.verticesBottomList[i+1], 8)
+        for i in range(len(self.terrain.verticesTopList)-1):
+            pygame.draw.line(self.screen,(255,0,0),self.terrain.verticesTopList[i], self.terrain.verticesTopList[i+1], 8)
+        pygame.draw.line(self.screen,(255,0,0),(15,0),(15,640),8)
 
+        for i in range(len(self.terrain.verticesBoxList)):
+            pygame.draw.rect(self.screen,(255,0,0),(self.terrain.verticesBoxList[i][0]-10,self.terrain.verticesBoxList[i][1]-10,20,20), 0)
+
+    def drawSubmarines(self):
+        verts = []
+        for sub in self.terrain.tabSub:
+            for v in sub.physicsPolygon.get_vertices():
+                x = v.rotated(sub.physicsPolygon.body.angle)[0] + sub.physicsPolygon.body.position[0]
+                y = v.rotated(sub.physicsPolygon.body.angle)[1] + sub.physicsPolygon.body.position[1]
+                verts.append((x,y))
+            if sub.isAlive:
+                pygame.draw.polygon(self.screen,sub.color,verts,0)
+                pygame.draw.circle(self.screen, sub.color, (int(sub.physicsPolygon.body.position.x + sub.sonarOffset[0]), int(sub.physicsPolygon.body.position.y + sub.sonarOffset[1])),sub.sonarRadius, 1)
+            verts = []
+    
+    
     def refresh(self):
         pygame.display.flip()
         self.screen.fill((0, 0, 0))
         self.terrain.update(120)
-        self.terrain.space.debug_draw(self.draw_options)
+        if DEBUG:
+            self.terrain.space.debug_draw(self.draw_options)
+        else:
+            self.drawLinesBoxes()
+            self.drawSubmarines()
+
         self.stats.draw(self.screen,self.terrain)
+
         #self.drawForces()
 
 #    def draw1(self):
