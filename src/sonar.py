026b58762a884b26b5c2a2146b43dc0722be2e66
PETITJEAN DAMIEN P1408987 <damien.petitjean@etu.univ-lyon1.fr>
2020-01-03 14:40:00 +0100

    Comment sonar.py

--- src/sonar.py
+++ src/sonar.py
@@ -1,6 +1,10 @@
 import pygame
 import pymunk
 
+
+## Classe sonar générant le sonar du sous marin
+## Param : Radius : Int
+##         tupleOffSet : Tuple d'int
 class Sonar:
     def __init__(self, radius, tupleOffSet):
         self.radius = radius
